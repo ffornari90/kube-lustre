4d4305b0436390f2be44806b6ff58b7473ff802c
Fornari <fornari@cnaf.infn.it>
2020-11-14 12:05:11 +0100

    added containerized setup

---
+++ non-containerized/dockerfiles/kube-lustre-configurator/kube-lustre-configurator.sh
@@ -0,0 +1,215 @@
+#!/bin/sh
+[ "$DEBUG" == "1" ] && set -x
+set -e
+
+CONFIG_DIR="${CONFIG_DIR:-/etc/kube-lustre}"
+CONFIGURATIONS_FILE="$CONFIG_DIR/configuration.json"
+DAEMONS_FILE="$CONFIG_DIR/daemons.json"
+CLIENTS_FILE="$CONFIG_DIR/clients.json"
+
+load_variables() {
+    NODE1_NAME="$(jq -r ".$CONFIGURATION[\"$DAEMON\"][0]" "$DAEMONS_FILE")"
+    NODE2_NAME="$(jq -r ".$CONFIGURATION[\"$DAEMON\"][1]" "$DAEMONS_FILE")"
+    NODE3_NAME="$(jq -r ".$CONFIGURATION[\"$DAEMON\"][2]" "$DAEMONS_FILE")"
+    NODE1_IP="$(getent ahosts "$NODE1_NAME" | grep -v -m1 ':' | awk '{print $1}')"
+    NODE2_IP="$(getent ahosts "$NODE2_NAME" | grep -v -m1 ':' | awk '{print $1}')"
+    LUSTRE="$(jq -r ".$CONFIGURATION | keys | contains([\"lustre\"])" "$CONFIGURATIONS_FILE")"
+    LUSTRE_FSNAME="$(jq -r ".$CONFIGURATION.lustre.fsname" "$CONFIGURATIONS_FILE")"
+    LUSTRE_DEBUG="$(jq -r ".$CONFIGURATION.lustre.debug" "$CONFIGURATIONS_FILE")"
+    LUSTRE_INSTALL="$(jq -r ".$CONFIGURATION.lustre.install" "$CONFIGURATIONS_FILE")"
+    LUSTRE_FORCE_CREATE="$(jq -r ".$CONFIGURATION.lustre.force_create" "$CONFIGURATIONS_FILE")"
+    LUSTRE_MGSNODE="$(jq -r ".$CONFIGURATION.lustre.mgsnode" "$CONFIGURATIONS_FILE")"
+    LUSTRE_DEVICE="$(jq -r ".$CONFIGURATION.lustre.device" "$CONFIGURATIONS_FILE")"
+    LUSTRE_MOUNTPOINT="$(jq -r ".$CONFIGURATION.lustre.mountpoint" "$CONFIGURATIONS_FILE")"
+    DRBD="$(jq -r ".$CONFIGURATION | keys | contains([\"drbd\"])" "$CONFIGURATIONS_FILE")"
+    DRBD_DEBUG="$(jq -r ".$CONFIGURATION.drbd.debug" "$CONFIGURATIONS_FILE")"
+    DRBD_INSTALL="$(jq -r ".$CONFIGURATION.drbd.install" "$CONFIGURATIONS_FILE")"
+    DRBD_FORCE_CREATE="$(jq -r ".$CONFIGURATION.drbd.force_create" "$CONFIGURATIONS_FILE")"
+    DRBD_DEVICE="$(jq -r ".$CONFIGURATION.drbd.device" "$CONFIGURATIONS_FILE")"
+    DRBD_PORT="$(jq -r ".$CONFIGURATION.drbd.port" "$CONFIGURATIONS_FILE")"
+    DRBD_SYNCER_RATE="$(jq -r ".$CONFIGURATION.drbd.syncer_rate" "$CONFIGURATIONS_FILE")"
+    DRBD_PROTOCOL="$(jq -r ".$CONFIGURATION.drbd.protocol" "$CONFIGURATIONS_FILE")"
+    DRBD_NODE1_DISK="$(jq -r ".$CONFIGURATION.drbd.disks[0]" "$CONFIGURATIONS_FILE")"
+    DRBD_NODE2_DISK="$(jq -r ".$CONFIGURATION.drbd.disks[1]" "$CONFIGURATIONS_FILE")"
+    NODE_LABEL="$LUSTRE_FSNAME/$DAEMON"
+    APP_NAME="$LUSTRE_FSNAME-$DAEMON"
+    LUSTRE_TYPE="$(echo "$DAEMON" | sed 's/[0-9]//g')"
+    LUSTRE_INDEX="$(echo "$DAEMON" | sed 's/[^0-9]//g')"
+
+    if [ "$DRBD" == "true" ]; then
+        LUSTRE_HA_BACKEND="drbd"
+        LUSTRE_SERVICENODE="${NODE1_NAME}:${NODE2_NAME}"
+    fi
+
+    [ "$LUSTRE_DEBUG" == "true" ] && LUSTRE_DEBUG="1" || LUSTRE_DEBUG="0"
+    [ "$DRBD_DEBUG"   == "true" ] && DRBD_DEBUG="1"   || DRBD_DEBUG="0"
+    [ "$LUSTRE_FORCE_CREATE" == "true" ] && LUSTRE_FORCE_CREATE="1" || LUSTRE_FORCE_CREATE="0"
+    [ "$DRBD_FORCE_CREATE"   == "true" ] && DRBD_FORCE_CREATE="1"   || DRBD_FORCE_CREATE="0"
+}
+
+echo "Parsing $CONFIGURATIONS_FILE"
+jq . "$CONFIGURATIONS_FILE"
+
+if [ "$CONFIGURE_SERVERS" == "1" ]; then
+    echo "Parsing $DAEMONS_FILE"
+    jq . "$DAEMONS_FILE"
+fi
+if [ "$CONFIGURE_CLIENTS" == "1" ]; then
+    echo "Parsing $CLIENTS_FILE"
+    jq . "$CLIENTS_FILE"
+fi
+
+# Check kubectl
+kubectl get nodes 1> /dev/null
+
+CONFIGURATIONS="$(jq -r '. | keys[]' "$DAEMONS_FILE")"
+
+# Checking configuration
+for CONFIGURATION in $CONFIGURATIONS; do
+
+    [ "$CONFIGURE_SERVERS" == "1" ] && DAEMONS="$(jq -r ".$CONFIGURATION | keys[]" "$DAEMONS_FILE")"
+    [ "$CONFIGURE_CLIENTS" == "1" ] && CLIENTS="$(jq -r ".$CONFIGURATION | keys[]" "$CLIENTS_FILE")"
+
+    for DAEMON in $DAEMONS; do
+
+        load_variables
+
+        if [ "$LUSTRE" == "false" ]; then
+            >&2 echo "Error: Lustre configuration $CONFIGURATION not found for DAEMON=$DAEMON"
+            exit 1
+        fi
+
+        if [ "$DRBD" == "true" ] && [ "$NODE3_NAME" != "null" ]; then
+            >&2 echo "Error: Only two nodes alowed for drbd configuration for DAEMON=$DAEMON"
+            exit 1
+        elif [ "$DRBD" == "false" ] && [ "$NODE2_NAME" != "null" ]; then
+            >&2 echo "Error: Only one node alowed for configuration without drbd for DAEMON=$DAEMON"
+            exit 1
+        fi
+
+        for i in NODE1_NAME NODE1_IP LUSTRE_FSNAME LUSTRE_INSTALL LUSTRE_MGSNODE LUSTRE_DEVICE LUSTRE_TYPE LUSTRE_INDEX; do
+            if [ -z "$(eval "echo \"\$$i"\")" ] || [ "$(eval "echo \"\$$i"\")" == "null" ]; then
+                >&2 echo "Error: variable $i is not specified for DAEMON=$DAEMON"
+                exit 1
+            fi
+        done
+
+        if [ "$DRBD" == "true" ]; then
+            for i in NODE2_NAME NODE2_IP DRBD_INSTALL DRBD_DEVICE DRBD_PORT  DRBD_NODE1_DISK DRBD_NODE2_DISK; do
+                if [ -z "$(eval "echo \"\$$i"\")" ] || [ "$(eval "echo \"\$$i"\")" == "null" ]; then
+                    >&2 echo "Error: variable $i is not specified for DAEMON=$DAEMON"
+                    exit 1
+                fi
+            done
+        fi
+
+        case "$LUSTRE_TYPE" in
+            ost ) : ;;
+            mdt ) : ;;
+            mgs ) : ;;
+            mdt-mgs ) : ;;
+            * )
+                >&2 echo "Error: variable LUSTRE_TYPE is specified wrong for DAEMON=$DAEMON"
+                >&2 echo "       LUSTRE_TYPE=<mgs|mdt|ost|mdt-mgs>"
+                exit 1
+            ;;
+        esac
+
+        if [ "${#LUSTRE_FSNAME}" -gt "8" ]; then
+            >&2 echo "Error: variable FSNAME cannot be greater than 8 symbols for DAEMON=$DAEMON, example:"
+            >&2 echo "       LUSTRE_FSNAME=lustre1"
+            exit 1
+        fi
+
+        NODES_BY_LABEL="$(kubectl get nodes -l "$NODE_LABEL=" -o json 2>/dev/null | jq -r '.items[].metadata.name' )"
+        if [ "$DRBD" == "true" ]; then
+            WRONG_NODES="$(echo "$NODES_BY_LABEL" | grep -v "^\(${NODE1_NAME}\|${NODE2_NAME}\)$" || true)"
+        else
+            WRONG_NODES="$(echo "$NODES_BY_LABEL" | grep -v "^${NODE1_NAME}$" || true)"
+        fi
+
+        if [ ! -z "$WRONG_NODES" ]; then
+            >&2 echo "Error: Wrong nodes nodes was found with label $NODE_LABEL:"
+            >&2 echo "     " $WRONG_NODES
+            exit 1
+        fi
+
+    done
+
+    for CLIENT in $CLIENTS; do
+
+        load_variables
+
+        if [ "$LUSTRE" == "false" ]; then
+            >&2 echo "Error: Lustre configuration $CONFIGURATION not found for CLIENT=$CLIENT"
+            exit 1
+        fi
+
+        for i in LUSTRE_MOUNTPOINT LUSTRE_FSNAME LUSTRE_INSTALL LUSTRE_MGSNODE; do
+            if [ -z "$(eval "echo \"\$$i"\")" ] || [ "$(eval "echo \"\$$i"\")" == "null" ]; then
+                >&2 echo "Error: variable $i is not specified for CLIENT=$CLIENT"
+                exit 1
+            fi
+        done
+
+    done
+
+done
+
+# Run configuration
+for CONFIGURATION in $CONFIGURATIONS; do
+
+    [ "$CONFIGURE_SERVERS" == "1" ] && DAEMONS="$(jq -r ".$CONFIGURATION | keys[]" "$DAEMONS_FILE")"
+
+    for DAEMON in $DAEMONS; do
+
+        load_variables
+
+        # label nodes
+        kubectl label node --overwrite "$NODE1_NAME" "$NODE_LABEL="
+        kubectl label node --overwrite "$NODE1_NAME" "$LUSTRE_FSNAME/server="
+        if [ "$DRBD" == "true" ]; then
+            kubectl label node --overwrite "$NODE2_NAME" "$NODE_LABEL="
+            kubectl label node --overwrite "$NODE2_NAME" "$LUSTRE_FSNAME/server="
+        fi
+
+        # apply drbd resources
+        if [ "$DRBD" == "true" ]; then
+            [ "$DRBD_SYNCER_RATE" == "null" ] && DRBD_SYNCER_RATE="5M"
+            [ "$DRBD_PROTOCOL" == "null" ] && DRBD_PROTOCOL="C"
+
+            if [ "$DRBD_INSTALL" == "true" ]; then
+                eval "echo \"$(cat drbd.yaml | sed 's/"/\\"/g' )\"" | kubectl apply -f -
+            else
+                eval "echo \"$(cat drbd.yaml | sed 's/"/\\"/g' )\"" | sed '/^ *initContainers: *$/,/^ *containers: *$/{/^ *containers: *$/!d}' | kubectl apply -f -
+            fi
+        fi
+
+        # apply lustre resources
+        if [ "$LUSTRE_INSTALL" == "true" ]; then
+            eval "echo \"$(cat lustre.yaml | sed 's/"/\\"/g' )\"" | kubectl apply -f -
+        elif [ "$LUSTRE_INSTALL" == "false" ]; then
+            eval "echo \"$(cat lustre.yaml | sed 's/"/\\"/g' )\"" | sed '/^ *initContainers: *$/,/^ *containers: *$/{/^ *containers: *$/!d}' | kubectl apply -f -
+        fi
+
+    done
+
+    if [ "$CONFIGURE_CLIENTS" == "1" ]; then
+        CLIENTS="$(jq -r ".$CONFIGURATION[]" "$CLIENTS_FILE")"
+
+        load_variables
+
+        for CLIENT in $CLIENTS; do
+            # label nodes
+            kubectl label node --overwrite "$CLIENT" "$LUSTRE_FSNAME/client="
+        done
+
+        # apply lustre client
+        if [ "$LUSTRE_INSTALL" == "true" ]; then
+            eval "echo \"$(cat lustre-client.yaml | sed 's/"/\\"/g' )\"" | kubectl apply -f -
+        elif [ "$LUSTRE_INSTALL" == "false" ]; then
+            eval "echo \"$(cat lustre-client.yaml | sed 's/"/\\"/g' )\"" | sed '/^ *initContainers: *$/,/^ *containers: *$/{/^ *containers: *$/!d}' | kubectl apply -f -
+        fi
+    fi
+
+done
---
+++ non-containerized/dockerfiles/lustre/lustre-wrapper.sh
@@ -0,0 +1,192 @@
+#!/bin/sh
+[ "$DEBUG" == "1" ] && set -x
+set -e
+
+for i in FSNAME DEVICE; do
+    if [ -z "$(eval "echo \"\$$i"\")" ]; then
+        >&2 echo "Error: variable $i is not specified"
+        exit 1
+    fi
+done
+
+MOUNT_DIR=${MOUNT_DIR:-/var/lib/lustre}
+[ "$FORCE_CREATE" == "1" ] && FORCE_CREATE_CMD="--force"
+
+case "$TYPE" in
+    ost )
+        TYPE_CMD="--ost"
+        POOL="${POOL:-$FSNAME-ost${INDEX}}"
+        NAME="${NAME:-ost${INDEX}}"
+    ;;
+    mdt )
+        TYPE_CMD="--mdt"
+        POOL="${POOL:-$FSNAME-mdt${INDEX}}"
+        NAME="${NAME:-mdt${INDEX}}"
+    ;;
+    mgs )
+        TYPE_CMD="--mgs"
+        POOL="${POOL:-$FSNAME-mds}"
+        NAME="${NAME:-mgs}"
+    ;;
+    mdt-mgs )
+        TYPE_CMD="--mdt --mgs"
+        POOL="${POOL:-$FSNAME-mdt${INDEX}-mgs}"
+        NAME="${NAME:-mdt${INDEX}-mgs}"
+    ;;
+    * )
+        >&2 echo "Error: variable TYPE is unspecified, or specified wrong"
+        >&2 echo "       TYPE=<mgs|mdt|ost|mdt-mgs>"
+        exit 1
+    ;;
+esac
+
+if [ "${#FSNAME}" -gt "8" ]; then
+    >&2 echo "Error: variable FSNAME cannot be greater than 8 symbols, example:"
+    >&2 echo "       FSNAME=lustre1"
+    exit 1
+else
+    FSNAME_CMD="--fsname=$FSNAME"
+fi
+
+if [ "$TYPE" != "mgs" ]; then
+    if [ -z "$INDEX" ]; then
+        >&2 echo "Error: variable INDEX is not specified, example:"
+        >&2 echo "       INDEX=1"
+        exit 1
+    else
+        INDEX_CMD="--index=$INDEX"
+    fi
+fi
+
+if ( [ "$TYPE" == "ost" ] || [ "$TYPE" == "mdt" ] ); then
+    if [ -z "$MGSNODE" ]; then
+        >&2 echo "Error: variable MGSNODE is not specified, example:"
+        >&2 echo "       MGSNODE=\"10.28.38.11@tcp,10.28.38.12@tcp\""
+        exit 1
+    else
+        MGSNODE_CMD="--mgsnode=$MGSNODE"
+    fi
+fi
+
+if [ "$HA_BACKEND" == "drbd" ]; then
+    case "" in
+        "$RESOURCE_NAME" )
+            >&2 echo "Error: variable RESOURCE_NAME is not specified for HA_BACKEND=drbd"
+            exit 1
+        ;;
+        "$SERVICENODE" )
+            >&2 echo "Error: variable SERVICENODE is not specified for HA_BACKEND=drbd, example:"
+            >&2 echo "       SERVICENODE=\"10.28.38.13@tcp,10.28.38.14@tcp\""
+            exit 1
+        ;;
+    esac
+    SERVICENODE_CMD="--servicenode=$SERVICENODE"
+fi
+
+if [ ! -z "$CHROOT" ]; then
+    DRBDADM="chroot $CHROOT drbdadm"
+    WIPEFS="chroot $CHROOT wipefs"
+    MODPROBE="chroot $CHROOT modprobe"
+    ZPOOL="chroot $CHROOT zpool"
+    SYSTEMCTL="chroot $CHROOT systemctl"
+    MKFS_LUSTRE="chroot $CHROOT mkfs.lustre"
+else
+    DRBDADM="drbdadm"
+    WIPEFS="wipefs"
+    MODPROBE="modprobe"
+    ZPOOL="zpool"
+    SYSTEMCTL="systemctl"
+    MKFS_LUSTRE="mkfs.lustre"
+fi
+
+# Check for module
+$MODPROBE zfs
+$MODPROBE lustre
+
+# Check for drbd resource
+if [ "$HA_BACKEND" == "drbd" ]; then
+    $DRBDADM status "$RESOURCE_NAME" 1>/dev/null
+fi
+
+# Create mount target
+MOUNT_TARGET="$MOUNT_DIR/$POOL/$NAME"
+SYSTEMD_UNIT="$(echo $MOUNT_TARGET | sed -e 's/-/\\x2d/g' -e 's/\//-/g' -e 's/^-//').mount"
+SYSTEMD_UNIT_FILE="$CHROOT/run/systemd/system/$SYSTEMD_UNIT"
+
+cleanup() {
+    [ "$?" != "0" ] && KUBE_NOTIFY=0
+    set +e
+
+    # kill tail process if exist
+    kill $TAILF_PID 2>/dev/null
+    # kill mount process if exist
+    kill -SIGINT $MOUNT_PID 2>/dev/null && wait $MOUNT_PID
+    # kill zpool process if exist
+    kill -SIGINT $ZPOOL_PID 2>/dev/null && wait $ZPOOL_PID
+
+    # umount lustre target if mounted
+    if $SYSTEMCTL is-active "$SYSTEMD_UNIT"; then
+        $SYSTEMCTL stop "$SYSTEMD_UNIT"
+    fi
+
+    rm -f "$SYSTEMD_UNIT_FILE"
+
+    # export zpool if imported
+    if $ZPOOL list "$POOL" &>/dev/null; then
+        $ZPOOL export -f "$POOL"
+    fi
+
+    # mark secondary if drbd backend
+    [ "$HA_BACKEND" == "drbd" ] && $DRBDADM secondary "$RESOURCE_NAME"
+
+    rmdir "$MOUNT_TARGET" 2>/dev/null
+
+    # kill the pod for not wait when Terminating are finished
+    [ "$KUBE_NOTIFY" == "1" ] && kubectl delete pod "$RESOURCE_NAME-0" --force --grace-period=0
+
+    exit 0
+}
+
+# Set exit trap
+trap cleanup SIGINT SIGHUP SIGTERM EXIT
+
+# Enable drbd primary
+if [ "$HA_BACKEND" == "drbd" ]; then
+    $DRBDADM primary "$RESOURCE_NAME"
+fi
+
+if ! $WIPEFS "$DEVICE" | grep -q "."; then
+    # Prepare drive
+    $MKFS_LUSTRE $FSNAME_CMD $FORCE_CREATE_CMD $MGSNODE_CMD $SERVICENODE_CMD $INDEX_CMD $TYPE_CMD --backfstype=zfs --force-nohostid "$POOL/$NAME" "$DEVICE"
+else
+    # Import zfs-pool
+    if ! $ZPOOL list | grep -q "^$POOL "; then
+        $ZPOOL import -o cachefile=none "$POOL" &
+        ZPOOL_PID=$!
+        wait $ZPOOL_PID
+    fi
+fi
+
+# Write unit
+cat > "$SYSTEMD_UNIT_FILE" <<EOT
+[Mount]
+What=$POOL/$NAME
+Where=$MOUNT_TARGET
+Type=lustre
+EOT
+
+# Start daemon
+$SYSTEMCTL daemon-reload
+if ! $SYSTEMCTL start "$SYSTEMD_UNIT"; then
+    # print error
+    $SYSTEMCTL status "$SYSTEMD_UNIT" | grep 'mount\[' | sed 's/^.*\]: //g' >&2
+    exit 1
+fi &
+
+MOUNT_PID=$!
+wait $MOUNT_PID
+
+# Sleep calm
+tail -f /dev/null &
+TAILF_PID=$!
+wait $TAILF_PID
