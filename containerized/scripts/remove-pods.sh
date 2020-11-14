120d260da24bcbe70f845054cb6a3bf7d25ab2e8
Fornari <fornari@cnaf.infn.it>
2020-11-15 00:10:04 +0100

    updated lustre containerized client docker and yaml files

--- containerized/scripts/remove-pods.sh
+++ containerized/scripts/remove-pods.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-kubectl delete daemonset -n lustre --all && kubectl delete sts -n lustre --all && kubectl delete -f ../yaml/kube-lustre-configurator.yaml
+kubectl delete daemonset -n lustre --all && kubectl delete -f ../yaml/kube-lustre-configurator.yaml
