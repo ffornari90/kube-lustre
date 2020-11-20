4ac47db7e10a4b6a057eb86f14d7542a27491601
Cloud User <centos@k8s-lustre-1.novalocal>
2020-11-20 23:50:24 +0000

    updated set-env.sh

--- containerized/dockerfiles/lustre-server/init.sh
+++ containerized/dockerfiles/lustre-server/init.sh
@@ -2,7 +2,7 @@
 /install-drbd.sh
 /install-lustre.sh
 modprobe -v osd_zfs
-modprobe -v mgs 
-modprobe -v mdt
+#modprobe -v mgs 
+#modprobe -v mdt
 #source /drbd-wrapper.sh
 #source /lustre-wrapper.sh
--- containerized/dockerfiles/lustre-server/set-env.sh
+++ containerized/dockerfiles/lustre-server/set-env.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
-export NODE1_NAME=k8s-lustre-1.novalocal
-export NODE1_IP=10.0.1.8
-export NODE2_NAME=k8s-lustre-2.novalocal
-export NODE2_IP=10.0.1.5
-export MGSNODE="10.0.1.8@eth0:10.0.1.5@eth0"
-export SERVICENODE="10.0.1.8@eth0:10.0.1.5@eth0"
+kubectl get pod -o=custom-columns=IP:.status.podIP,NAME:.metadata.name -n lustre | grep "${FSNAME}" | tee /etc/hosts
+nodes=(`kubectl get pod -o=custom-columns=NAME:.metadata.name,IP:.status.podIP -n lustre | grep "${TYPE%-*}" | awk '{print $1}'`)
+ips=(`kubectl get pod -o=custom-columns=NAME:.metadata.name,IP:.status.podIP -n lustre | grep "${TYPE%-*}" | awk '{print $2}'`)
+export NODE1_NAME="${nodes[0]}"
+export NODE1_IP="${ips[0]}"
+export NODE2_NAME="${nodes[1]}"
+export NODE2_IP="${ips[1]}"
+export MGSNODE="${ips[0]}@eth0:${ips[1]}@eth0"
+export SERVICENODE="${ips[0]}@eth0:${ips[1]}@eth0"
 ip=$(ifconfig eth0 | grep inet | awk '$1=="inet" {print $2}')
 netmask=$(ifconfig eth0 | grep inet | awk '$1=="inet" {print $4}')
 broadcast=$(ifconfig eth0 | grep inet | awk '$1=="inet" {print $6}')
+network=$(ip route show | grep "${ip}" | awk '{print $1}')
+network="${network%/*}"
 printf 'DEVICE=eth0
 IPADDR=%s
 NETMASK=%s
-NETWORK=10.244.0.0
+NETWORK=%s
 BROADCAST=%s
 ONBOOT=yes
 NAME=ether\n' \
-"${ip}" "${netmask}" "${broadcast}" | tee /etc/sysconfig/network-scripts/ifcfg-eth0
+"${ip}" "${netmask}" "${network}" "${broadcast}" | tee /etc/sysconfig/network-scripts/ifcfg-eth0
 touch /etc/sysconfig/network
+systemctl restart network
+systemctl status network
 modprobe -v lnet
 lnetctl lnet configure
-systemctl restart network
+lnetctl net show
